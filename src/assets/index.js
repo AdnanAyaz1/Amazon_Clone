8537de28f6580d3dc5eec530a97e09dd4eddb755
AdnanAyaz1 <20pwcse1860@uetpeshawar.edu.pk>
2024-01-09 17:22:22 +0500

    push

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,11 @@
 
 import logo from './logo.png';
-import bannerImgone from './banner/bannerImgone.jpg'
-import bannerImgTwo from './banner/bannerImgTwo.jpg'
-import bannerImgThree from './banner/bannerImgThree.jpg'
-import bannerImgFour from './banner/bannerImgFour.jpg'
-import bannerImgFive from './banner/bannerImgFive.jpg'
+import bannerimgone from './banner/bannerimgone.jpg'
+import bannerimgTwo from './banner/bannerimgtwo.jpg'
+import bannerimgThree from './banner/bannerimgthree.jpg'
+import bannerimgFour from './banner/bannerimgfour.jpg'
+import bannerimgFive from './banner/bannerimgfive.jpg'
 import emptyCart from './emptyCart.png'
 
-const images = [bannerImgone,bannerImgTwo,bannerImgThree,bannerImgFour,bannerImgFive]
+const images = [bannerimgone,bannerimgTwo,bannerimgThree,bannerimgFour,bannerimgFive]
 export {logo,images,emptyCart};
